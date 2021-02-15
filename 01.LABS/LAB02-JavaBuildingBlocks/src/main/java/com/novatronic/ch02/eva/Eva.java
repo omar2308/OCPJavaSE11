f3e00b9bf0554d61e7cb5c4897086ddb543910e2
Omar Fernandez <omarfl7@gmail.com>
2021-02-15 16:44:34 -0500

    Se agrega avance de clase y correción de encabezado del material ch03, y ejemplos realizados en clase

---
+++ 01.LABS/LAB02-JavaBuildingBlocks/src/main/java/com/novatronic/ch02/eva/Eva.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.novatronic.ch02.eva;
+
+/**
+ *
+ * @author Omar
+ */
+public class Eva {
+    public static void main(String[] args) {
+        var day = 1/2;
+        System.out.println("day:" + day);
+        float d = 3.0F;
+        long x = 9999999999L;
+        
+        byte b = 12*10;  //Para el compilador esto es 120
+        int e = 999999999;
+        short c = 999;
+    }
+}
---
+++ 01.LABS/LAB03-Operators/src/main/java/com/novatronic/ch03/Promotion.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.novatronic.ch03;
+
+/**
+ *
+ * @author Omar
+ */
+public class Promotion {
+    public static void main(String[] args) {
+        float x = 3;
+        int y = 4;
+        //short tail = (short)4 + y;
+        float egg = 2.0f / 9;
+        
+        int tadpole = 5 * (int)2L;
+        
+        long reptile = 192301398193810323L;
+    }
+}
